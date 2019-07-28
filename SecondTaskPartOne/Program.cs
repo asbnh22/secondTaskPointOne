4fc47c646e600fd44791273fcd725507a4c237e5
anb <borodina-a.b@yandex.ru>
2019-07-29 00:24:13 +0300

    Add project files.

---
+++ SecondTaskPartOne/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SecondTaskPartOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
